22b43051991e0b51b9d24a2991b5fddba5cfbf50
loranm <chmoxster@gmail.com>
2016-05-11 19:46:31 +0200

    Gestion des collisions intégrée à la méthode move()

--- animation_piece/animation-fork5.js
+++ animation_piece/animation-fork5.js
@@ -12,7 +12,20 @@ window.document.addEventListener('DOMContentLoaded', function(){
 ******************************************************************************/
   var mySpriteSheet= new Image();
   mySpriteSheet.src = 'img/mySpriteSheet.png';
+  //charge le background
+  function background(){
+    var fondImage = new Image();
+    fondImage.src = "img/titlescreen-panorama01.jpg"
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(fondImage,0,0);
+  };
 /******************************************************************************/
+
+/******************************************************************************
+      Ce tableau stocke tout ce qui peut blesser le héros
+*******************************************************************************/
+  var danger = [];
+
 
 
 /******************************************************************************
@@ -34,20 +47,26 @@ window.document.addEventListener('DOMContentLoaded', function(){
       this.dw = options.dw;
       this.dh = options.dh;
       this.ratio = options.ratio;
-      this.frameIndex = 0,
+      this.frameIndex = 0;
+      this.danger = options.danger;
 
-    this.render = function(){ //affiche l'image dans le canvas
-        this.context.drawImage(
-          this.image, //insert l'image
-          this.sx + this.sw * this.frameIndex,
-          this.sy,
-          this.sw, // slice l'image en X
-          this.sh,
-          this.dx,
-          this.dy,
-          this.dw * this.ratio,
-          this.dh * this.ratio);
-    };
+
+      this.render = function(){ //affiche l'image dans le canvas
+          this.context.drawImage(
+            this.image, //insert l'image
+            this.sx + this.sw * this.frameIndex,
+            this.sy,
+            this.sw, // slice l'image en X
+            this.sh,
+            this.dx,
+            this.dy,
+            this.dw * this.ratio,
+            this.dh * this.ratio);
+      };
+
+      if(this.danger){
+        danger.push(this)
+      }
   };
 
 
@@ -74,6 +93,19 @@ window.document.addEventListener('DOMContentLoaded', function(){
 
 //Fonction constructeur qui déplace le sprite sur le canvas
   var MovingSprite = function(options){
+    this.testCollision = function(){
+      for (var i = 0; i < danger.length; i++){
+        if (this.dx < danger[i].dx + danger[i].dw &&
+          this.dx + this.dw > danger[i].dx &&
+          this.dy < danger[i].dy + danger[i].dh &&
+          this.dh + this.dy > danger[i].dy){
+            console.log('collision détectée');
+            this.dx = 10;
+          }
+        }
+      };
+
+
     this.moveTo = options.moveTo;
     this.moveFrom = options.moveFrom;
     this.move = function(){ //Effectue une translation de l'image
@@ -83,7 +115,7 @@ window.document.addEventListener('DOMContentLoaded', function(){
     if(this.dx < this.moveFrom){
       this.dir = 0;
     }
-    this.dx = (this.dir) ? (this.dx - 5) : (this.dx + 5);
+    this.dx = (this.dir) ? (this.dx - 4) : (this.dx + 4);
   };
   }
 
@@ -110,13 +142,6 @@ window.document.addEventListener('DOMContentLoaded', function(){
 
 
 
-  //charge le background
-  function background(){
-    var fondImage = new Image();
-    fondImage.src = "img/titlescreen-panorama01.jpg"
-    var ctx = canvas.getContext('2d');
-      ctx.drawImage(fondImage,0,0);
-  };
 
   //Animation des sprites
   function gameLoop() {
@@ -124,14 +149,14 @@ window.document.addEventListener('DOMContentLoaded', function(){
     background();
       coin.update();
       coin.render();
-      // coin.move();
 
       claw.render()
-      console.log(claw);
+      // console.log(claw);
 
 
       tim.update();
       tim.render();
+      tim.testCollision();
       tim.move();
 
     window.requestAnimationFrame(gameLoop);
@@ -153,16 +178,17 @@ window.document.addEventListener('DOMContentLoaded', function(){
     sy : 0,
     sw: 100,
     sh: 100,
-    dx : 500,
+    dx : 700,
     dy : 370,
     dw : 100,
     dh : 100,
-    ratio : 0.8
+    ratio : 0.8,
+    danger: true
   });
 
 
   //Instanciation d'une pièce
-  var coin = getMovingSprite({
+  var coin = getAnimatedSprite({
     context: canvas.getContext('2d'),
     sx : 0,
     sy : 0,
@@ -193,8 +219,8 @@ window.document.addEventListener('DOMContentLoaded', function(){
     numberOfFrames : 27,
     ticksPerFrame : 4,
     moveFrom: 10,
-    moveTo: 500
+    moveTo: 700
   });
-
+console.log(danger[0].dx)
 
 });
